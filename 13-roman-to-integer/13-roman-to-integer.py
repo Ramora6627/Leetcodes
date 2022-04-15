38321e85bc475f508fc5ca6fbbb8283208833587
Ramora6627 <96392044+Ramora6627@users.noreply.github.com>
2022-04-15 19:06:13 -0400

    Time: 65 ms (55.85%), Space: 13.8 MB (98.77%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.py
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -1,18 +1,34 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        roman_dict = {"I":1, "V":5, "X": 10, "L":50, "C": 100, "D":500, "M": 1000 }
-        exceptions = {"IV" : 4, "IX" : 9, "XL": 40, "XC": 90, "CD":400 , "CM":900}
-        total = 0
-        i = 0
-        while i < len(s) : 
-            if i != s[-1]:
-                combined = s[i:i+2:1]
-                if combined in exceptions.keys():
-                    total += exceptions[combined]
-                    i += 2
-                else:
-                    total += roman_dict[s[i]]
-                    i += 1
+#         roman_dict = {"I":1, "V":5, "X": 10, "L":50, "C": 100, "D":500, "M": 1000 }
+#         exceptions = {"IV" : 4, "IX" : 9, "XL": 40, "XC": 90, "CD":400 , "CM":900}
+#         total = 0
+#         i = 0
+#         while i < len(s) : 
+#             if i != s[-1]:
+#                 combined = s[i:i+2:1]
+#                 if combined in exceptions.keys():
+#                     total += exceptions[combined]
+#                     i += 2
+#                 else:
+#                     total += roman_dict[s[i]]
+#                     i += 1
+#             else:
+#                 total += roman_dict[s[i]]
+#         return total
+    
+#         Solution 2:
+        d = {"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
+        nums = list(map(lambda x:d[x],s))
+        print(nums)
+        ans, carry = 0, 0
+
+        for num in nums:
+            if num <= carry:
+                ans += carry
+                carry = num
             else:
-                total += roman_dict[s[i]]
-        return total
+                ans -= carry
+                carry = num
+        
+        return ans+carry
