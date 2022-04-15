8eb19475e1e82d37c6d4f3f0bf2a66fcfae085b2
Ramora6627 <96392044+Ramora6627@users.noreply.github.com>
2022-04-15 18:55:17 -0400

    Time: 55 ms (74.96%), Space: 13.9 MB (33.56%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -0,0 +1,18 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman_dict = {"I":1, "V":5, "X": 10, "L":50, "C": 100, "D":500, "M": 1000 }
+        exceptions = {"IV" : 4, "IX" : 9, "XL": 40, "XC": 90, "CD":400 , "CM":900}
+        total = 0
+        i = 0
+        while i < len(s) : 
+            if i != s[-1]:
+                combined = s[i:i+2:1]
+                if combined in exceptions.keys():
+                    total += exceptions[combined]
+                    i += 2
+                else:
+                    total += roman_dict[s[i]]
+                    i += 1
+            else:
+                total += roman_dict[s[i]]
+        return total
