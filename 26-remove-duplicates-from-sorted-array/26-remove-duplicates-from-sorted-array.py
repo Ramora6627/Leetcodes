44e78fcd271db9576d526426a7ea972fcae8e6bd
Ramora6627 <96392044+Ramora6627@users.noreply.github.com>
2022-04-15 22:27:44 -0400

    Time: 238 ms (10.61%), Space: 15.5 MB (64.63%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.py
@@ -1,12 +1,40 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        i = 1
+        # i = 1
+        # while i in range(len(nums)):
+        #     if nums[i] == nums[i-1] and type(nums[i]) == int:
+        #         nums.remove(nums[i])
+        #         nums.extend("N") 
+        #         i = i
+        #     else:
+        #         i += 1
+        # x = {"N"}
+        # return len(set(nums).difference(x))
+        lst = []
+        # for i in range(1,len(nums)):
+        #     if nums[i] == nums[i-1] and type(nums[i]) == int:
+        #         nums.remove(nums[i])
+        #         nums.extend("N") 
+        #         i = i
+        #     else:
+        #         i += 1
+        # x = {"N"}
+        #         # x = {"N"}
+        # return len(set(nums).difference(x))   
+        i = 0
         while i in range(len(nums)):
-            if nums[i] == nums[i-1] and type(nums[i]) == int:
+            if nums[i] not in lst:
+                lst.append(nums[i])
+                i += 1      
+            elif nums[i] in lst and type(nums[i]) == int:
                 nums.remove(nums[i])
-                nums.extend("N") 
-                i = i
-            else:
-                i += 1
-        x = {"N"}
+        nums.extend(repeat("x",len(lst)))
+        # while i in range(len(nums)):
+        #     if nums[i] == nums[i-1] and type(nums[i]) == int:
+        #         nums.remove(nums[i])
+        #         nums.extend("N") 
+        #         i = i
+        #     else:
+        #         i += 1
+        x = {"x"}
         return len(set(nums).difference(x))
