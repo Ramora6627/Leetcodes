908efed22ac4a167cc097cc3c08942aaddfa68eb
Ramora6627 <96392044+Ramora6627@users.noreply.github.com>
2022-04-18 19:06:43 -0400

    Time: 819 ms (72.12%), Space: 28.5 MB (10.04%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -54,25 +54,33 @@ class Solution:
 #                 print("x",total)
 #                 print("y",max_end_here)
 #         return total
-        best_sum = float('-inf')
-        best_start = best_end = 0  # or: None
-        current_sum = 0
-        for current_end, x in enumerate(nums):
-            if current_sum <= 0:
-                # Start a new sequence at the current element
-                current_start = current_end
-                current_sum = x
-            else:
-                # Extend the existing sequence with the current element
-                current_sum += x
+#         best_sum = float('-inf')
+#         best_start = best_end = 0  # or: None
+#         current_sum = 0
+#         for current_end, x in enumerate(nums):
+#             if current_sum <= 0:
+#                 # Start a new sequence at the current element
+#                 current_start = current_end
+#                 current_sum = x
+#             else:
+#                 # Extend the existing sequence with the current element
+#                 current_sum += x
 
-            if current_sum > best_sum:
-                best_sum = current_sum
-                best_start = current_start
-                best_end = current_end + 1  # the +1 is to make 'best_end' match Python's slice convention (endpoint excluded)
+#             if current_sum > best_sum:
+#                 best_sum = current_sum
+#                 best_start = current_start
+#                 best_end = current_end + 1  # the +1 is to make 'best_end' match Python's slice convention (endpoint excluded)
 
-        return best_sum
+#         return best_sum
+        max_so_far =nums[0]
+        curr_max = nums[0]
 
+        for i in range(1,len(nums)):
+            curr_max = max(nums[i], curr_max + nums[i])
+            max_so_far = max(max_so_far,curr_max)
+
+        return max_so_far
+ 
 #         max_so_far = nums[0]
 #         max_ending_here = 0
 
