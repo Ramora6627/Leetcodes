b4ec96aa7cca37b2fb4e208d3ba97375897be500
Ramora6627 <96392044+Ramora6627@users.noreply.github.com>
2022-05-01 22:25:49 -0400

    Time: 43 ms (48.08%), Space: 13.8 MB (68.16%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.py
+++ 118-pascals-triangle/118-pascals-triangle.py
@@ -1,18 +1,18 @@
 class Solution:
     def generate(self, n: int) -> List[List[int]]:
         # only up to n = 5: return [list(str(11**(i-1))) for i in range (1, numRows+1)]
-#         res = [[1]]
+        res = [[1]]
         
-#         for i in range (numRows-1):
-#             temp = [0] + res[-1] + [0]
-#             row = []
-#             for j in range (len(res[-1]) + 1):
-#                 row.append(temp[j]+ temp[j+1])
-#             res.append(row)    
-#         return res
+        for i in range (n-1):
+            temp = [0] + res[-1] + [0]
+            row = []
+            for j in range (len(res[-1]) + 1):
+                row.append(temp[j]+ temp[j+1])
+            res.append(row)    
+        return res
 
-        ans = [[1]*i for i in range(1, n+1)]   # initialize triangle with all 1
-        for i in range(1, n):
-            for j in range(1, i):
-                ans[i][j] = ans[i-1][j] + ans[i-1][j-1] # update each as sum of two elements from above row
-        return ans
+        # ans = [[1]*i for i in range(1, n+1)]   # initialize triangle with all 1
+        # for i in range(1, n):
+        #     for j in range(1, i):
+        #         ans[i][j] = ans[i-1][j] + ans[i-1][j-1] # update each as sum of two elements from above row
+        # return ans
