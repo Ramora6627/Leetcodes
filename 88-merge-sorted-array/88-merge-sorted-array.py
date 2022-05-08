7b480111fe66f0bf149efc597214c9d1e2c50888
Ramora6627 <96392044+Ramora6627@users.noreply.github.com>
2022-05-08 19:54:04 -0400

    Time: 45 ms (64.19%), Space: 13.9 MB (86.71%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.py
+++ 88-merge-sorted-array/88-merge-sorted-array.py
@@ -1,5 +1,32 @@
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        # m = len(nums1)
+        # n = len(nums2)
+        i = m-1
+        j = n-1
+        k = i+j +1 
+        
+        nums1[m:] = nums2[:n]
+        # print (i,j,k)
+        while k >=0:
+            # print (i,j,k)
+            if i>=0 and j>=0 and nums2[j] <=  nums1[i]:
+                nums1[k] = nums1[i]
+                # print (nums1)
+                i -= 1 
+            # elif nums2[j] == nums1[i]:
+            #     nums1[k] = nums1[i]
+            #     nums1.pop(j+i)
+            #     j -= 1
+            #     i -= 1 
+            #     print (nums1)
+            elif j>=0:
+                nums1[k] = nums2[j]
+                j-=1
+                # print (nums1)
+            k -= 1
+        # print (nums1)
+        return nums1
         # """
         # Do not return anything, modify nums1 in-place instead.
         # """
@@ -15,12 +42,27 @@ class Solution:
         # nums1[:] = sorted(nums1[:m]+nums2[:n])
     
 # solution 3:
-        nums1[m:] = nums2  
-        nums1 = nums1.sort()
+#         nums1[m:] = nums2  
+#         nums1 = nums1.sort()
         
         # return nums1[len(nums1)-(n+m):]
             # nums1 = nums1[(m-n):] + nums1[:(m-n)]
             # print(nums1)
             # return nums1
         # return nums1
+
+#         nums1[m:] = nums2[:n]
+#         print (nums1)
+#         while m>1:
+#             if nums1[m-1] < nums1[m]:
+#                 return nums1
+#             elif nums1[m-1] > nums1[m]:
+#                 print(nums1[m-1])
+#                 nums1.insert(m+n-1,nums1[m-1])
+#                 nums1.remove(nums1[m-1])
+#                 # nums1[-1] = nums1[m-1]
+#                 print (nums1)
+#                 m -= 1
+#         return nums1
+
         
