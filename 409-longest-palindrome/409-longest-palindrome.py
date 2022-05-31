9861cd783ba17cd015ed9a7077c8e41c45d5ea97
Ramora6627 <96392044+Ramora6627@users.noreply.github.com>
2022-05-30 23:47:38 -0400

    Time: 27 ms (62.17%), Space: 13.7 MB (12.41%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.py
+++ 409-longest-palindrome/409-longest-palindrome.py
@@ -5,14 +5,14 @@ class Solution(object):
         :rtype: int
         """
         count = 0
-        odd = 0
+        odd = False
         for char in set(s):
             if s.count(char)%2 == 0:
                 count += s.count(char)
             else:
                 count += s.count(char)-1
-                odd += 1
-        if odd > 0:
+                odd = True
+        if odd:
             count += 1
 
         return count
