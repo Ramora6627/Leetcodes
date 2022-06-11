46c7210d09016f96d11d6af322fd83dbccce68a8
Ramora6627 <96392044+Ramora6627@users.noreply.github.com>
2022-06-11 15:26:08 -0400

    Time: 1895 ms (28.53%), Space: 15 MB (76.17%) - LeetHub

---
+++ 1-two-sum/1-two-sum.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        res = []
+        for num in nums:
+            residual = target - num
+            if residual in res and num != residual:
+                return ([nums.index(residual), nums.index(num)])
+            elif residual in res and num == residual:
+                a = nums.index(residual)
+                b = nums[a+1:]
+                print (a)
+                return [a,b.index(num)+len(nums[:a+1])]
+
+            res.append(num)
