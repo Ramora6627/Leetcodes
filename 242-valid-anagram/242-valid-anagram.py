2187768cd9ea52dcd35761bd91f236394cb93c0f
Ramora6627 <96392044+Ramora6627@users.noreply.github.com>
2022-06-11 18:00:49 -0400

    Time: 100 ms (14.72%), Space: 15.1 MB (21.55%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,5 +1,6 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        return sorted(s) == sorted(t)
 #         if len(s) != len(t):
 #             return False
 #         hashtable = {}
@@ -16,15 +17,16 @@ class Solution:
 #         return len(new) == len(s)
 
 
-        if len(s) != len(t):
-            return False
+#         if len(s) != len(t):
+#             return False
         
-        countS, countT = {}, {}
+#         countS, countT = {}, {}
         
-        for i in range(len(s)):
-            countS[s[i]] = 1 + countS.get(s[i], 0)
-            countT[t[i]] = 1 + countT.get(t[i], 0)
-        for c in countS:
-            if countS[c] != countT.get(c, 0):
-                return False
-        return True
+#         for i in range(len(s)):
+#             countS[s[i]] = 1 + countS.get(s[i], 0)
+#             countT[t[i]] = 1 + countT.get(t[i], 0)
+#         for c in countS:
+#             if countS[c] != countT.get(c, 0):
+#                 return False
+#         return True
+
