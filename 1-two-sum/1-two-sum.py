c0935fa971e47248e6effd9c5a995caf0cf3fde1
Ramora6627 <96392044+Ramora6627@users.noreply.github.com>
2022-06-11 16:01:02 -0400

    Time: 121 ms (41.85%), Space: 15 MB (76.11%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,14 +1,23 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        res = []
-        for num in nums:
-            residual = target - num
-            if residual in res and num != residual:
-                return ([nums.index(residual), nums.index(num)])
-            elif residual in res and num == residual:
-                a = nums.index(residual)
-                b = nums[a+1:]
-                print (a)
-                return [a,b.index(num)+len(nums[:a+1])]
+#         res = []
+#         for num in nums:
+#             residual = target - num
+#             if residual in res and num != residual:
+#                 return ([nums.index(residual), nums.index(num)])
+#             elif residual in res and num == residual:
+#                 a = nums.index(residual)
+#                 b = nums[a+1:]
+#                 print (a)
+#                 return [a,b.index(num)+len(nums[:a+1])]
 
-            res.append(num)
+#             res.append(num)
+            
+            
+        prevMap = {} # val -> index
+        
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[n] = i
