3792f577f587836b360ac87143bcb156c3c1c38c
Ramora6627 <96392044+Ramora6627@users.noreply.github.com>
2022-06-11 14:58:17 -0400

    Time: 85 ms (28.92%), Space: 14.5 MB (65.75%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        hashtable = {}
+        
+        for letter in set(s):
+            hashtable[letter] = s.count(letter)
+        new = ""
+        for st in t:
+            if st not in hashtable:
+                return False
+            elif st in hashtable and hashtable[st] != 0:
+                new += st
+                hashtable[st] -= 1
+        return len(new) == len(s)
