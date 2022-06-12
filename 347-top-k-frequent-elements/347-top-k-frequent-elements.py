adb6c8f286f9c0428cf9adb3023ae330b7108d5e
Ramora6627 <96392044+Ramora6627@users.noreply.github.com>
2022-06-11 20:55:22 -0400

    Time: 4113 ms (5.09%), Space: 18.7 MB (68.91%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -3,23 +3,41 @@ class Solution:
         hashtable = {}
         output = []
         for num in set(nums):
-            if num not in hashtable:
-                hashtable[num] = nums.count(num)
-        # print (hashtable)
-        c = hashtable.copy()
-        # print(c)
-        while hashtable:
-            for key,val in c.items():
-                if not hashtable.values():
-                    break
-                # print(key,val)
-                # print(max(hashtable.values()))
-                # print(hashtable.values())
-                if val == max(hashtable.values()):
-                    output.append(key)
-                    del hashtable[key]
-                    # print(hashtable)
-        # print(output)
-        return output[:k]
-                
+            if nums.count(num) not in hashtable:
+                hashtable[nums.count(num)] = [num]
+            else:
+                hashtable[nums.count(num)].append(num)
+        # print(hashtable)
+        v = sorted(hashtable.keys(),reverse=True)
+        # print(v)
+        i = 0
+        while len(output) != k:
+            # print(hashtable[v[i]])
+            for j in range(len(hashtable[v[i]])):
+                output.append(hashtable[v[i]][j])
+                # print(output)
+            if len(output) == k:
+                break
+            i += 1
+        return output
+#         hashtable = {}
+#         output = []
+#         for num in set(nums):
+#             if num not in hashtable:
+#                 hashtable[num] = nums.count(num)
+
+#         c = hashtable.copy()
+
+#         while hashtable:
+#             for key,val in c.items():
+#                 if not hashtable.values():
+#                     break
+
+#                 if val == max(hashtable.values()):
+#                     output.append(key)
+#                     del hashtable[key]
+
+
+#         return output[:k]
+        
         
