d215a0a126c83b130f9fa19882d2903befad227a
Ramora6627 <96392044+Ramora6627@users.noreply.github.com>
2022-06-11 22:08:25 -0400

    Time: 241 ms (87.93%), Space: 21 MB (96.00%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -1,21 +1,33 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        hashtable = {}
-        answer = []
-        for num in nums:
-            hashtable[num] = 1 + hashtable.get(num, 0)
-        # print(hashtable)           
-        for num in nums:
-            p = 1
-            hashtable[num] -= 1
-            for key,val in hashtable.items():
-                if val != 0:
-                    p = p*(key**val); 
-                    # print(p)
-            answer.append(p)
-            hashtable[num]+= 1
-        return answer
-            
+        # hashtable = {}
+        # answer = []
+        # for num in nums:
+        #     hashtable[num] = 1 + hashtable.get(num, 0)
+        # # print(hashtable)           
+        # for num in nums:
+        #     p = 1
+        #     hashtable[num] -= 1
+        #     for key,val in hashtable.items():
+        #         if val != 0:
+        #             p = p*(key**val); 
+        #             # print(p)
+        #     answer.append(p)
+        #     hashtable[num]+= 1
+        # return answer
+        
+        
+        res = [1] * (len(nums))
+        
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+        postfix = 1
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+        return res
 #         for i in range(len(nums)):
 #             for j in range(len(nums)):
 #                 if j != i:
