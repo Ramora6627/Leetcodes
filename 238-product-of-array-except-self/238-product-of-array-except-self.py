521484f400a875faa941de0b40522d92e50c176d
Ramora6627 <96392044+Ramora6627@users.noreply.github.com>
2022-06-11 21:33:42 -0400

    Time: 431 ms (15.10%), Space: 21.2 MB (55.62%) - LeetHub

---
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -0,0 +1,26 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        hashtable = {}
+        answer = []
+        for num in set(nums):
+            hashtable[num] = nums.count(num)
+        print(hashtable)           
+        for num in nums:
+            p = 1
+            hashtable[num] -= 1
+            for key,val in hashtable.items():
+                if val != 0:
+                    p = p*(key**val); 
+                    # print(p)
+            answer.append(p)
+            hashtable[num]+= 1
+        return answer
+            
+#         for i in range(len(nums)):
+#             for j in range(len(nums)):
+#                 if j != i:
+#                     p = p*nums[j]
+#             answer.append(p)
+#             p = 1
+#         return answer
+                
