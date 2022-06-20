ef0d8f86397c12dc3ba97ea350f46cbbf3dc7c30
Ramora6627 <96392044+Ramora6627@users.noreply.github.com>
2022-06-19 22:56:31 -0400

    Time: 124 ms (18.95%), Space: 14.7 MB (88.93%) - LeetHub

--- 345-reverse-vowels-of-a-string/345-reverse-vowels-of-a-string.py
+++ 345-reverse-vowels-of-a-string/345-reverse-vowels-of-a-string.py
@@ -5,14 +5,11 @@ class Solution:
         for char in s:
             if char.lower() in vowels:
                 reverse_list.append(char)
-        print(reverse_list)
         index = 0
         reverse_list[::] = reverse_list[::-1]
-        print(reverse_list)
         new = ""
         for char in s:
             if char.lower() in vowels:  
-                # print(index)
                 new += reverse_list[index]
                 index += 1
             else:
