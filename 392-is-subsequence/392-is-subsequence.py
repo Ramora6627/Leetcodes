0493b06f7bf99c0f306ea778547fb065b58c5a21
Ramora6627 <96392044+Ramora6627@users.noreply.github.com>
2022-06-26 18:01:48 -0400

    Time: 33 ms (92.50%), Space: 13.8 MB (81.52%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.py
+++ 392-is-subsequence/392-is-subsequence.py
@@ -1,21 +1,28 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        if t == "" and s == "":
-            return True
-        if t == "":
-            return False
-        lst = list(t)
-        output = []
-        for i in range(len(s)):
-            if i == 0:
-                if s[i] in lst:
-                    output.append(lst.index(s[i]))
-                    lst[lst.index(s[i])] ="-"
-            elif s[i] in lst[output[-1]:]:
-                # print(output[-1:])
-                output.append(lst[output[-1]:].index(s[i])+output[-1])
-                lst[lst.index(s[i])] ="-"
+#         if t == "" and s == "":
+#             return True
+#         if t == "":
+#             return False
+#         lst = list(t)
+#         output = []
+#         for i in range(len(s)):
+#             if i == 0:
+#                 if s[i] in lst:
+#                     output.append(lst.index(s[i]))
+#                     lst[lst.index(s[i])] ="-"
+#             elif s[i] in lst[output[-1]:]:
+#                 # print(output[-1:])
+#                 output.append(lst[output[-1]:].index(s[i])+output[-1])
+#                 lst[lst.index(s[i])] ="-"
 
-        if len(output) == len(s) and sorted(output) == output:
-            return True
-        return False
+#         if len(output) == len(s) and sorted(output) == output:
+#             return True
+#         return False
+        i,j = 0,0
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:
+                i += 1
+            j += 1
+
+        return i == len(s)
