b5b131949a53cf7450724903567aa1338e443387
Ramora6627 <96392044+Ramora6627@users.noreply.github.com>
2022-06-26 17:51:23 -0400

    Time: 43 ms (69.59%), Space: 14.1 MB (12.41%) - LeetHub

---
+++ 392-is-subsequence/392-is-subsequence.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        if t == "" and s == "":
+            return True
+        if t == "":
+            return False
+        lst = list(t)
+        output = []
+        for i in range(len(s)):
+            if i == 0:
+                if s[i] in lst:
+                    output.append(lst.index(s[i]))
+                    lst[lst.index(s[i])] ="-"
+            elif s[i] in lst[output[-1]:]:
+                print(output[-1:])
+                output.append(lst[output[-1]:].index(s[i])+output[-1])
+                lst[lst.index(s[i])] ="-"
+
+                # print(lst)
+        print(output,sorted(output))
+        if len(output) == len(s) and sorted(output) == output:
+            return True
+        return False
