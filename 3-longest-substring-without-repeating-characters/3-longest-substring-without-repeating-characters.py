bd06e68214f640808b49ddd6e114c39a4f4f5730
Ramora6627 <96392044+Ramora6627@users.noreply.github.com>
2022-07-02 14:32:10 -0400

    Time: 1498 ms (5.00%), Space: 14.2 MB (13.30%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -3,14 +3,11 @@ class Solution:
         i = 0
         j = 0
         output = 0
-        hashtable = {}
         for i in range(len(s)):
             while j<len(s) and s[j] not in s[i:j]:
                 j += 1
-            print(i,j)
             if len(s[i:j])> output:
                 output = len(s[i:j])
-                print(s[i:j],output)
              
             i += 1
             j = i +1
