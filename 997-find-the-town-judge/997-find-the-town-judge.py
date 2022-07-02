4e7c832592eb745767576615836c87f75e10ffcb
Ramora6627 <96392044+Ramora6627@users.noreply.github.com>
2022-07-02 16:46:24 -0400

    Time: 1172 ms (37.99%), Space: 19.2 MB (7.45%) - LeetHub

--- 997-find-the-town-judge/997-find-the-town-judge.py
+++ 997-find-the-town-judge/997-find-the-town-judge.py
@@ -2,24 +2,23 @@ class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
         if n == 1:
             return 1
-        if not trust and n != 1:
-            return -1
-        candidate = {}
+        a = set()
+        hashtable = {}
         for pair in trust:
-            if pair[0] not in candidate:
-                candidate[pair[0]] = [pair[1]]
+            if pair[1] not in hashtable:
+                hashtable[pair[1]] = [pair[0]]
+                a.add(pair[0])
             else:
-                candidate[pair[0]].append(pair[1])
-        
-        for key,value in candidate.items():
-            candidate[key] = set(value)
-        
-        if set.intersection(*candidate.values()):
-            common = set.intersection(*candidate.values())
-            u = set.union(*candidate.values())
-            a = list(common)[0]
-            if len(common)==1 and a not in candidate and len(candidate.keys()) == n-1:
-                        return a
+                hashtable[pair[1]].append(pair[0])
+                a.add(pair[0])
 
-        return -1
+        print (hashtable,a)
+        town = []
+        for key,value in hashtable.items():
+            if len(value) == n-1 and key not in a:
+                town.append(key)
         
+        if len(town) == 1:
+            return town[0]
+        
+        return -1
