0ec1f4ee1803200e614c1008d04ba8f002e0f99b
Ramora6627 <96392044+Ramora6627@users.noreply.github.com>
2022-07-04 16:55:23 -0400

    Time: 60 ms (15.36%), Space: 13.8 MB (62.14%) - LeetHub

--- 1816-truncate-sentence/1816-truncate-sentence.py
+++ 1816-truncate-sentence/1816-truncate-sentence.py
@@ -1,16 +1,4 @@
 class Solution:
     def truncateSentence(self, s: str, k: int) -> str:
-        space = 0
-        output = ""
-        while space<k:
-            for char in s:
-                if char == " " and space<k:
-                    output += char
-                    space += 1
-                elif space<k:
-                    # print (char,space)
-                    output += char
-            if space==k-1:
-                break
-        return output.strip()
+        return " ".join(list(s.split())[:k])
             
