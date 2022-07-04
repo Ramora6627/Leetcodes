7ecd7ad8da6ce9103562a8785e74a3ab47449440
Ramora6627 <96392044+Ramora6627@users.noreply.github.com>
2022-07-04 01:03:37 -0400

    Time: 481 ms (25.61%), Space: 25.3 MB (75.29%) - LeetHub

--- 189-rotate-array/189-rotate-array.py
+++ 189-rotate-array/189-rotate-array.py
@@ -3,14 +3,28 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        k = k%len(nums)
-        temp = []
-        for i in range(0,len(nums)-k):
-            temp.append(nums[i])
-        # print(temp)
-        for i in range(len(nums)-k,len(nums)):
-            # print(i,nums[i])
-            nums[(i+k)%len(nums)] = nums[i]
-            # print(nums)
+#         k = k%len(nums)
+#         temp = []
+#         for i in range(0,len(nums)-k):
+#             temp.append(nums[i])
+#         # print(temp)
+#         for i in range(len(nums)-k,len(nums)):
+#             # print(i,nums[i])
+#             nums[(i+k)%len(nums)] = nums[i]
+#             # print(nums)
         
-        nums[k:]=temp[:]
+#         nums[k:]=temp[:]
+        k = k%len(nums)
+        l,r = 0,len(nums)-1
+        while l<r:
+            nums[l],nums[r]=nums[r],nums[l]
+            l,r = l+1,r-1
+        l,r = 0,k-1
+        while l<r:
+            nums[l],nums[r]=nums[r],nums[l]
+            l,r = l+1,r-1
+        l,r = k,len(nums)-1
+        while l<r:
+            nums[l],nums[r]=nums[r],nums[l]
+            l,r = l+1,r-1
+        
