425e513d05cbfaafdb422ad06d9ce660d880ef32
Ramora6627 <96392044+Ramora6627@users.noreply.github.com>
2022-07-05 00:11:15 -0400

    Time: 290 ms (56.34%), Space: 14.3 MB (89.20%) - LeetHub

--- 1447-simplified-fractions/1447-simplified-fractions.py
+++ 1447-simplified-fractions/1447-simplified-fractions.py
@@ -1,3 +1,9 @@
 class Solution:
     def simplifiedFractions(self, n: int) -> List[str]:
-        return [f"{x}/{d}" for d in range(1, n+1) for x in range(1, d) if gcd(x, d) == 1]
+        p = []
+        for d in range(1,n+1):
+            for x in range(1,d):
+                if gcd(x,d) == 1:
+                    p.append(f"{x}/{d}")
+        return p
+
