182072a00bbc5e14219b9603585159af2acd2f61
Ramora6627 <96392044+Ramora6627@users.noreply.github.com>
2022-07-09 00:31:24 -0400

    Time: 89 ms (86.00%), Space: 14.2 MB (42.09%) - LeetHub

--- 1460-make-two-arrays-equal-by-reversing-sub-arrays/1460-make-two-arrays-equal-by-reversing-sub-arrays.py
+++ 1460-make-two-arrays-equal-by-reversing-sub-arrays/1460-make-two-arrays-equal-by-reversing-sub-arrays.py
@@ -1,19 +1,3 @@
-class Solution(object):
-    def canBeEqual(self, target, arr):
-        """
-        :type target: List[int]
-        :type arr: List[int]
-        :rtype: bool
-        """
-        # for num in  arr:
-#             if not num in target or arr.count(num) > target.count(num):
-#                 return False
-#                 break
-                
-#         return True
-
-        # target.sort()
-        # arr.sort()
-        # return target == arr 
-    
-        return collections.Counter(target) == collections.Counter(arr)
+class Solution:
+    def canBeEqual(self, target: List[int], arr: List[int]) -> bool:
+        return sorted(target) == sorted(arr)
